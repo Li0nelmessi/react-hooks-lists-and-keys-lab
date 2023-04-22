c26cc43197c6a0c75bea638725e37862bf7e9b8e
Lionel <wafulalionel20@gmail.com>
2023-04-23 01:42:21 +0300

    Complete Assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,6 +4,17 @@ function NavBar() {
   const links = ["home", "about", "projects"];
 
   return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const link = links.map((link) => {
+    return (
+      <nav>
+        {/* display an <a> tag for each link here */}
+        <a href={`#$link`} key={link}>
+          {link}{" "}
+        </a>
+      </nav>
+    );
+  });
+  return <nav>{link}</nav>;
 }
 
 export default NavBar;
